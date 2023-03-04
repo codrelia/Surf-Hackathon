1bd023d4329cf31d99fd7c137f97cfe3f374707a
yaprincev <5489812@mail.ru>
2023-03-04 16:59:02 +0300

    add keyboard

--- Template/Modules/KeyBoardEntering/KeyBoardViewController.swift
+++ Template/Modules/KeyBoardEntering/KeyBoardViewController.swift
@@ -7,19 +7,34 @@
 
 import UIKit
 
-class KeyBoardViewController: UIViewController {
+class KeyBoardViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var confirmButton: UIButton!
     @IBOutlet weak var textCode: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        textCode.delegate = self
         confirmButton.setTitle("Узнать информацию", for: .normal)
         textCode.attributedPlaceholder = NSAttributedString(string: "Введите штрих-код")
         // Do any additional setup after loading the view.
     }
-
-
-    /*
+    
+    @IBAction func textAction(_ sender: Any) {
+        textCode.becomeFirstResponder()
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("test")
+        view.endEditing(true)
+        textCode.resignFirstResponder()
+    }
+    @IBAction func buttonAction(_ sender: Any){
+        view.endEditing(true)
+        textCode.resignFirstResponder()
+    }
+    
+}
+/*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -29,4 +44,4 @@ class KeyBoardViewController: UIViewController {
     }
     */
 
-}
+
